8c2a0d60ff03e89ab11fd6190edaa0fef0b391f1
Chris <moorecash@gmail.com>
2022-03-16 17:44:00 -0400

    rails helper addition

--- my_goal_app/spec/views/rails_helper.rb
+++ my_goal_app/spec/views/rails_helper.rb
@@ -7,4 +7,22 @@ require 'capybara/rspec'
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 
+ActiveRecord::Migration.maintain_test_schema!
 
+RSpec.configure do |config|
+  config.include AuthFeaturesHelper, type: :feature
+  config.include GoalFeaturesHelper, type: :feature
+  config.include CheerFeaturesHelper, type: :feature
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  
+  config.use_transactional_fixtures = true
+
+  config.infer_spec_type_from_file_location!
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+end

bbf4dea6fe57bf3a60f6fac02e629ebfb207c439
Chris <moorecash@gmail.com>
2022-03-16 16:45:23 -0400

    get new

--- my_goal_app/spec/controllers/goals_controller_spec.rb
+++ my_goal_app/spec/controllers/goals_controller_spec.rb
@@ -6,3 +6,28 @@ RSpec.describe GoalsController, type: :controller do
   before(:each) do
     allow_message_expectations_on_nil
   end
+
+  describe 'get #new' do
+    context 'when logged in' do
+      before do
+        allow(controller).to receive(:current_user) { jack }
+      end
+
+      it 'renders the new goals page' do
+        get :new
+        expect(response).to render_template('new')
+      end
+    end
+
+
+    context 'when logged out' do
+      before do
+        allow(controller).to receive(:current_user) { nil }
+      end
+
+      it 'redirects to the login page' do
+        get :new
+        expect(response).to redirect_to(new_user_url)
+      end
+    end
+  end

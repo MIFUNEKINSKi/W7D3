7aef0da7f341e203b4475d12b5fbaece843544cf
Chris <moorecash@gmail.com>
2022-03-16 17:07:57 -0400

    start post create

--- my_goal_app/spec/controllers/goals_controller_spec.rb
+++ my_goal_app/spec/controllers/goals_controller_spec.rb
@@ -31,3 +31,32 @@ RSpec.describe GoalsController, type: :controller do
       end
     end
   end
+
+   describe 'POST #create' do
+    let(:jack_bruce) { User.create!(username: 'jack_bruce', password: 'abcdef') }
+
+    context 'when logged out' do
+      before do
+        allow(controller).to receive(:current_user) { nil }
+      end
+
+      it 'redirects to the login page' do
+        post :create, params: { goal: {} }
+        expect(response).to redirect_to(new_user_url)
+      end
+    end
+
+    context 'when logged in' do
+      before do
+        allow(controller).to receive(:current_user) { jack_bruce }
+      end
+
+      context 'with invalid params' do
+        it 'validates the presence of title and url' do
+          post :create, params: { goal: { title: 'invld' } }
+          expect(response).to render_template('new')
+          expect(flash[:errors]).to be_present
+        end
+      end
+
+ 
